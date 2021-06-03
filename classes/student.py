e1bdb1af27bb1d527fb4846c7c8b74ff76f05826
Smattonline <smattonline@gmail.com>
2021-06-03 01:59:26 -0700

    Musa's School interface Two

--- classes/student.py
+++ classes/student.py
@@ -19,3 +19,9 @@ class Student(Person):
                 students.append(Student(**dict(row)))
         return students
 
+    def __str__(self, student_id):
+        self.students = Student.objects()
+        for student in self.students:
+            if student.school_id == student_id:
+                print(f"{student.name}\n\{student.age}\n\............................\n\{student.school_id}")
+
